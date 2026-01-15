bf6198eea99f5676c8b059f9788481fd199df38a
peterhutas <peter.hutas@metlife.com>
2026-01-15 22:45:47 +0100

    Commited missing vendor files

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2185d2f99bcd56787481d9357a5972d3::getLoader();
+return ComposerAutoloaderInitf3c041c275dfadf82cb64fb19eab2eb9::getLoader();
